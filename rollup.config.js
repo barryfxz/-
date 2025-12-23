0b3e81503a73242e1964cb377c59e5cef85da2ed
barryfxz <159066418+barryfxz@users.noreply.github.com>
2025-12-23 06:20:28 +0100

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,1 +1,10 @@
+import { terser } from "@rollup/plugin-terser";
 
+export default {
+  input: "src/index.jsx",
+  output: {
+    file: "dist/bundle.js",
+    format: "iife",
+  },
+  plugins: [terser()],
+};
